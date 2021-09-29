0e864c1c4f617785538433b0ceb23fc24570fa46
drad <robertjdstoddard@gmail.com>
2021-09-29 13:54:22 -0600

    server file added

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,9 +2,7 @@ const router = require('express').Router()
 const thoughtRoutes = require('./thought-routes')
 const userRoutes = require('./user-routes')
 
-router.use('/thoughts')
-router.use('/users')
-router.use('/reaction')
-router.use('/friends')
+router.use('/users', userRoutes)
+router.use('/friends', thoughtRoutes)
 
 module.exports = router
